9eb36790f1727024e239113b226ecf48887c0464
Frederik Moroder <frotti92@hotmail.com>
2020-11-14 22:18:22 +0100

    Carousel Idea

--- src/Components/Carousel/Carousel.tsx
+++ src/Components/Carousel/Carousel.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
-import { useMemo, useState } from 'react'
+import { useMemo, useState, useRef } from 'react'
 import ItemCard from '../ItemCard'
+import './Carousel.scss'
 import { Categories } from '../../Utils/Categories'
 
 export interface CarouselProps {
@@ -11,10 +12,43 @@ const Carousel: React.FC<CarouselProps> = (props: CarouselProps) => {
     const [displayArray, setDisplayArray] = useState(
         Math.round(Categories.length / 2)
     )
-
+    const [counter, setCounter] = useState<number>(
+        Math.round(Categories.length / 2)
+    )
+    const carouselSlide = useRef<HTMLDivElement>(null)
+    React.useEffect(() => {
+        const allCards: NodeListOf<HTMLDivElement> = document.querySelectorAll(
+            '.card'
+        )
+        const size = allCards[0].offsetHeight
+        if (carouselSlide.current) {
+            console.log(
+                carouselSlide.current,
+                size,
+                -Categories.length * size,
+                allCards[0]
+            )
+            carouselSlide.current.style.transform =
+                'translateY(' + -Categories.length * (size + 40) + 'px)'
+            //Increment en med trans
+            styleCarousel(counter, allCards)
+            onTransitionEnd(counter, carouselSlide.current)
+            styleCarousel(counter, allCards)
+        }
+    }, [counter, carouselSlide])
+    const onTransitionEnd = (index: number, el: HTMLDivElement) => {
+        //sæt tilbage uden trans
+    }
+    const styleCarousel = (
+        index: number,
+        Elements: NodeListOf<HTMLDivElement>
+    ) => {
+        //styles center kort og de omkring liggende
+    }
     return (
         <div
             className="carousel-slide"
+            ref={carouselSlide}
             style={{ flexDirection: props.horizontal ? 'row' : 'column' }}
         >
             {Categories.length && (
