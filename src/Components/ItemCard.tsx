c1573652d5818b45cd8636e300aca2737d4249c1
BAD MOTHERFUCKER <lassestausgaard@gmail.com>
2020-11-16 15:45:08 +0100

    Clean up

--- src/Components/ItemCard.tsx
+++ src/Components/ItemCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ItemCard.scss'
 import { menuItem } from '../App'
 
@@ -15,77 +15,40 @@ export interface ItemCardProps {
 
 const ItemCard: React.FC<ItemCardProps> = (props) => {
   const { type, name, imgSrc, price, item, toggleModal, addItemToOrder, orderSelection } = props;
+  const layout = type !== 'item' ? 'text-container' : 'text-container-centered';
   const route = './products/' + imgSrc;
 
-  switch (type) {
-    case 'category':
-      // Menu card for selecting category
-      return (
-        <div className="card">
-          <img src={route} alt=''></img>
-          <div className='text-container'>
-            <span>{name.toUpperCase()}</span>
-          </div>
-        </div>
-      )
-    case 'item':
-      if (toggleModal && item?.type === 'burger') {
-        // Card for selecting a burger and opening modal
-        return (
-          <div 
-            className="card"
-            onClick={() => toggleModal(true, item)}>
-              <img src={route} alt=''></img>
-              <div className='text-container-centered'>
-                <span>{item.name.toUpperCase()}</span>
-                { price 
-                  ? <span>{item.price}DKK</span>
-                  : null
-                }
-              </div>
-          </div>
-        )
-      } else if (addItemToOrder && item) {
-        // Card for selecting !burger and adding it to the order
-        return (
-          <div 
-            className="card"
-            onClick={() => addItemToOrder(item)}>
-              <img src={route} alt=''></img>
-              <div className='text-container-centered'>
-                <span>{item.name.toUpperCase()}</span>
-                { price 
-                  ? <span>{item.price}DKK</span>
-                  : null
-                }
-              </div>
-          </div>
-        )
-      }
-      break;
-    case 'order selection':
-      if (orderSelection) {
-        // Modal card for selecting 'single' or 'menu' option
-        return (
-          <div 
-            className="card"
-            onClick={() => orderSelection(name)}>
-              <img src={route} alt=''></img>
-              <div className='text-container'>
-                <span>{name.toUpperCase()}</span>
-              </div>
-          </div>
-        )
-      }
-      break;
-    default:
-      return (
-        <></>
-      )
+  const onClickAction = () => {
+    switch (type) {
+      case 'category':
+        break;
+      case 'item':
+        if(toggleModal && item?.type === 'burger') {
+          toggleModal(true, item);
+        } else if(addItemToOrder && item) {
+          addItemToOrder(item);
+        }
+        break;
+      case 'order selection':
+        if(orderSelection) {
+          orderSelection(name);
+        }
+    }
   }
 
   return (
-    <></>
+    <div 
+      className="card"
+      onClick={() => onClickAction()}>
+        <img src={route} alt=''></img>
+        <div className={layout}>
+          <span>{name.toUpperCase()}</span>
+          { price 
+            ? <span>{price}DKK</span>
+            : null
+          }
+        </div>
+    </div>
   )
 }
 
